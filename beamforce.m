39c085668455a8427bbd03dc3ebd76cdb5098a14
derekjanoski <79541455+derekjanoski@users.noreply.github.com>
2021-02-23 12:16:38 -0800

    Add files via upload
    Uploading initial main.m script and beamforce.m function files. These files currently work together and can accurately accelerate a 2D circle via force from either a uniform beam, a gaussian beam, or a multimodal gaussian beam.

---
+++ beamforce.m
@@ -0,0 +1,68 @@
+function F = beamforce(R,P,PROFILE,X,Y)
+%BEAMFORCE(R,P,PROFILE,X,Y) Calculates force on a spherical due to the
+%photon momentum from an incoming laser beam. R is the radius of the
+%spherical sail, P is the total power output of the laser, PROFILE is a
+%string that idnetifies the laser beam profile
+
+
+c = 3e8; % speed of light [m/s]
+
+N = length(X);
+dy = 2*R/N;
+std = R/sqrt(2*log(2));
+switch PROFILE
+    case 'uniform'
+        profile = ones(1,N)./N; % creating a uniform distribution for the laser profile
+    case 'gaussian'
+        profile = normpdf(Y,0,std);
+        % Maybe ask on piazza about how to determine half beam width from std?
+    case 'multiple gaussian'
+        profile = normpdf(Y,.5*R,std) + normpdf(Y,-.5*R,std);
+end
+
+PVec = P*profile; % Assigning the power for each individual ray
+% figure(2); plot(PVec,Y,P*normpdf(Y,.5*R,R),Y,P*normpdf(Y,-.5*R,R),Y)
+
+bHat = [1 0]; % b^ vector. Ie, direction of beam propagation
+
+% Finding the normal vector for every point on the sail
+dxVec = gradient(X); dyVec = gradient(Y);
+   
+F = 0;
+% For loop used to calculate the force due to the beam
+for j = 1:length(Y)
+    % Calling normal vector for the point of interest
+    nHat = [dyVec(j) -dxVec(j)]/norm([dyVec(j) -dxVec(j)]);
+    % Calculating force due to the ray of interest
+    Fnow = 2*PVec(j)*dot(bHat,nHat)*nHat*dy/c;
+    % Adding this ray's force to the total force
+    F = F + Fnow;
+    
+    %{
+    THE FORCE EQUATION COULD BE WRONG...
+    IT DOESN'T HAVE CONSISTENT UNITS!!!
+    CORRECT UNITS WOULD NEED DIVISION BY SOME AREA.
+    FROM ONLINE RESOURCES, THE FORCE WOULD BE AS FOLLOWS:
+    FOR A PERFECT REFLECTOR
+    F = 2*u;
+    u = INT(I/A);
+    I (FOR A GUASSIAN BEAM) = 2*P*S/(c*PI*W^2)
+    WHERE W IS THE BEAM WIDTH AND S IS THE SURFACE AREA.
+    %}
+        
+    % Plotting every 100th ray and its respective normal and force
+    % vectors
+%     figure(1); 
+    hold on
+    if mod(j,100) == 1 
+        ray = [-2*R X(j); Y(j) Y(j)];
+        plot(ray(1,:), ray(2,:), 'r')
+%         quiver(X(j), Y(j), nHat(1), nHat(2), .25, 'm')
+%         quiver(X(j), Y(j), Fnow(1), Fnow(2), 1e3, 'b')
+   end
+end
+F = F/pi/((2*R)^2);
+
+
+end
+
---
+++ main.m
@@ -0,0 +1,78 @@
+%{
+AME 441 Group 5
+Passive Stability of a Laser-Driven Light Sail
+Spring 2021
+%}
+
+clear; clc; close all;
+
+%% Setting up inital paramters and known constants
+
+m = 1;                  % Mass of the sail [kg]
+R = 1;                  % Radius of sail [m]
+center = [0 0];         % Initial position of sail center
+v = [0 0];              % Initial velocity of sail [m/s]
+P = 30;                 % Power of laser beam [W]
+N = 501;                % Number of rays
+profile = 'multiple gaussian';   % Type of beam profile 
+dt = 1;                 % time differential for force calculation [s]
+
+myPlt = figure(1);      % Creating a figure to plot sail anf beam
+t = 0;                  % initiating time
+
+%% While loop used to update sail posisiton over time
+while t <= 3.15e7 % letting code run for a "year"
+
+    % Calculating x and y points on the sail for plotting and force
+    % calulations
+    th = linspace(.5*pi,1.5*pi,N);
+    yVec = R*sin(th) + center(2); %linspace(-R,R,N); Ignore these, just ideas
+    xVec = R*cos(th) + center(1); %center(1) - sqrt( R^2 - (yVec - center(2)).^2 );
+    xVec2 = R*cos(th+pi) + center(1); %center(1) + sqrt( R^2 - (yVec - center(2)).^2 );
+    % xVec2 is a dummy vector used only for plotting since the force is
+    % only on the left side of the sail
+    
+    % Plotting most recent sail position
+    myPlt; hold off
+    plot(xVec,yVec,'k', xVec2, yVec, 'k'), axis equal,
+    xlim([center(1)-4*R, center(1)+4*R])
+    ylim([center(2)-2*R, center(2)+2*R])    
+
+%% Calculating total force and plotting   
+    
+%{  
+So we have a seperate scripts that calculate different forces (drag and
+ from the beam) that we can call here. That way we make sure things are
+kept seperate and can run by themselves first. Just so nothing gets too
+hairy.
+%}  
+    FBeam = beamforce(R,P,profile,xVec,yVec);
+    FDrag = 0; % dragforce()
+    F = FBeam + FDrag;
+  
+    % Plotting total force vector at the sails "center of mass"
+    hold on, quiver(center(1), center(2), F(1), F(2), inv(norm(F)),...
+        'b', 'LineWidth', 2)
+    hold on, plot(center(1),center(2), 'b*', 'LineWidth', 5)
+    drawnow
+    
+    % Calculating the accelleration due to the force
+    a = F./m;
+    % Updating the velocity due to the acceleration over the time
+    % differential
+    v = v + a.*dt;
+    % Updating the position due to the velocity over the time differential
+    center = center + v.*dt;
+    
+    % A bunch of print statements just to see what is going on
+    fprintf('\nt = %i:%i:%.2f\n', floor(t/3600),...
+        floor((t-(floor(t/3600)*3600))/60), ...
+        floor( (t-(floor(t/3600)*3600)-(floor((t-(floor(t/3600)*3600))/60)*60))))        
+    fprintf('F = %e N\n', F)
+    fprintf('a = %e m/s^2\n', a)
+    fprintf('v = %f m/s\n', v)
+    fprintf('x = %f m\n', center)
+    
+%     pause()
+    t = t + dt; % Updating time so the while loop ends at some point.
+end
