15e1dafa7f0413ab69e941ec32b8e5662c95594e
jnesson <79543325+jnesson@users.noreply.github.com>
2021-03-10 11:43:41 -0800

    Update dragforce.m
    deleted some comments

--- dragforce.m
+++ dragforce.m
@@ -8,10 +8,6 @@ if nargin < 1
     error('Too few inputs')
 end
 
-%check to see if numeric vector value
-% if ~isnumeric(v) || ~isvector(v)
-%     error('Input must be numeric vector value')
-% end
 
 A = pi*R^2; % cross-sectional area of the sail
 C_D = 2.667; % drag coefficient for diffuse reflection of a sphere from SMAD
