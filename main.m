ab2df839c0e19c472448ec00cbe6183d434afc8d
derekjanoski <79541455+derekjanoski@users.noreply.github.com>
2021-05-07 13:19:19 -0700

    Update main.m
    Added torque calculations and functionality to add a COM offeset

--- main.m
+++ main.m
@@ -8,6 +8,7 @@ clear; clc; close all;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Create "Real-Time" Plots? 1 == Yes, 0 == No
 plt = 0;
+%% Create final plots?
 PLTs = 1;
 %% Create "Real-Time" Print Statements? 1 == Yes, 0 == No
 stmnt = 0;
@@ -16,7 +17,10 @@ stmnt = 0;
 % 1 for include, 0 for don't
 Drag = 0;
 PointingTol = 0;
-BeamDivergence = 1;
+BeamDivergence = 0;
+% SurfaceRoughness = 0;
+% Define the offset in [x y]
+CenterOfMassOffset = [1e-3 1e-3];
 
 %% Setting up inital paramters and known constants
 
@@ -24,17 +28,19 @@ m = 1;                  % Mass of the sail [kg]
 R = .1;                 % Radius of sail [m]
 center = [0 0];         % Initial position of sail center
 v = [0 0];              % Initial velocity of sail rel to beam sat [m/s]
+omega = 0;              % Initial angular rate of sail
+theta = 0;              % Inital rotation due to torques
 P = 30;                 % Power of laser beam [W]
 lambda = 980e-9;        % Wavelength of laser
-N = 1e6;               % Number of rays --> KEEP EVEN
+N = 1e3;               % Number of rays --> KEEP EVEN
 if mod(N,2) ~= 0
     error('N must be even to create power distribution.')
 end
 profile = 'multi-mode gaussian';    % Type of beam profile 
 dt = 3600;                 % time differential for force calculation [s]
-tTotal = 2*7*24*3600;      % total time to run the experiment
+tTotal = (16*24 + 10)*3600;      % total time to run the experiment
 rho = 5.12e-19;         % atmospheric density at GEO (35,786km altitude) [kg/m^3]
-if PointingTol == 0     % Pointing accuracy of laser sat (assumed)
+if PointingTol == 1     % Pointing accuracy of laser sat (assumed)
     tol = .2*pi/180;    % Taken from ITU-R
 else
     tol = 0;
@@ -54,6 +60,9 @@ if PLTs == 1
     offsetVec = Fvec;
 end
 
+COM = center + CenterOfMassOffset;
+% Fmag = 1;
+% Finit = 1.2572e-7;
 %% While loop used to update sail posisiton over time
 while t <= tTotal % letting code run for a "year". May change to a few weeks
 
@@ -62,8 +71,8 @@ while t <= tTotal % letting code run for a "year". May change to a few weeks
         
     yVec = linspace(center(2)-R,center(2)+R,N);
 
-    xVec = center(1) - sqrt( R^2 - (yVec - center(2)).^2 ); %center(1) - R.*cos(thetaVec);
-    xVec2 = center(1) + sqrt( R^2 - (yVec - center(2)).^2 ); %center(1) + R.*cos(thetaVec);
+    xVec = center(1) - ((sqrt( R^2 - (yVec - center(2)).^2).^4).^.25); %center(1) - R.*cos(thetaVec);
+    xVec2 = center(1) + ((sqrt( R^2 - (yVec - center(2)).^2).^4).^.25); %center(1) + R.*cos(thetaVec);
     % xVec2 is a dummy vector used only for plotting since the force is
     % only on the left side of the sail
     if ~isreal(yVec)
@@ -93,8 +102,8 @@ kept seperate and can run by themselves first. Just so nothing gets too
 hairy.
 %}  
 
-    FBeam = beamforce(R,P,lambda,profile,tol,xVec,yVec,center(1), ...
-        BeamDivergence, plt);
+    [FBeam,tau] = beamforce(R,P,lambda,profile,tol,xVec,yVec,...
+        center(1), BeamDivergence, plt, COM);
     if Drag == 1
         FDrag = dragforce(rho,norm(v),R);
     else
@@ -113,6 +122,23 @@ hairy.
         hold on, plot(center(1),center(2), 'b*', 'LineWidth', 5)
         drawnow
     end
+ 
+     %{
+    TODO: Add rotation from torques
+    Either use T = (mr^2)alpha or Euler's Eq
+    %}
+    
+    % Assuming thin spherical shell with negligible (uniform) mass
+    % distribution
+    I = (2/3)*m*R^2; 
+    alpha = tau/I;
+    omega = omega + alpha*dt;
+    theta = mod(theta+omega*dt,2*pi);
+    
+    % Rotating surface about COM by theta
+    Q = [cos(theta) -sin(theta);sin(theta) cos(theta)];
+    center = Q*center' + (eye(2)-Q)*COM';
+    center = center';   
     
     % Calculating the accelleration due to the force
     a = F./m;
@@ -121,6 +147,7 @@ hairy.
     v = v + a.*dt;
     % Updating the position due to the velocity over the time differential
     center = center + v.*dt;
+    COM = COM + v.*dt;
     
     if PLTs == 1
         velVec(counter) = v(1);
@@ -136,10 +163,20 @@ hairy.
         floor( (t-(floor(t/3600)*3600)-(floor((t-(floor(t/3600)*3600))/60)*60))))
     
     if stmnt == 1
-        fprintf('F = %e N\n', F)
-        fprintf('a = %e m/s^2\n', a)
-        fprintf('v = %f m/s\n', v)
-        fprintf('x = %f m\n', center)
+        fprintf('Fx = %e N\n', F(1))
+        fprintf('Fy = %e N\n', F(2))
+        fprintf('ax = %e m/s^2\n', a(1))
+        fprintf('ay = %e m/s^2\n', a(2))
+        fprintf('vx = %f m/s\n', v(1))
+        fprintf('vy = %f m/s\n', v(2))
+        fprintf('x = %f m\n', center(1))
+        fprintf('y = %f m\n', center(2))
+        fprintf('tau = %e Nm\n', tau)
+        fprintf('alpha = %e rad/s^2\n', alpha)
+        fprintf('omega = %f rad/s\n', omega)
+        fprintf('theta = %f rad\n', theta)
+        fprintf('COMx = %f m\n', COM(1))
+        fprintf('COMy = %f m\n', COM(2))
     end
     
     %% Checking if the sail falls off of the beam and breaks if so
@@ -151,20 +188,21 @@ hairy.
         break
     end
     
+%     Fmag = norm(F);
 %     pause()
     t = t + dt; % Updating time so the while loop ends at some point.
     counter = counter + 1; % Updating interation number
 end
 
 if PLTs == 1
-    figure, plot(tVec,disVec)
-    xlabel('Time [s]'), ylabel('Distance [m]')
-    figure, plot(tVec,velVec)
-    xlabel('Time [s]'), ylabel('Velocity [m/s]')
-    figure, plot(tVec,Fvec)
-    xlabel('Time [s]'), ylabel('Force [N]')
+    figure, plot(tVec./24./3600,disVec)
+    xlabel('Time [Days]'), ylabel('Distance [m]')
+    figure, plot(tVec./24./3600,velVec)
+    xlabel('Time [Days]'), ylabel('Velocity [m/s]')
+    figure, plot(tVec./24./3600,Fvec)
+    xlabel('Time [Days]'), ylabel('Force [N]')
     figure, plot(disVec, Fvec)
     xlabel('Distance [m]'), ylabel('Force [N]')
-    figure, plot(tVec, offsetVec)
-    xlabel('Time [s]'), ylabel('Sail Offset from Beam Axis [m]')
+    figure, plot(tVec./24./3600, offsetVec)
+    xlabel('Time [Days]'), ylabel('Sail Offset from Beam Axis [m]')
 end
