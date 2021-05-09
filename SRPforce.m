12a8453a48c182eefe58d04bd49ca2c7af88337e
jnesson <79543325+jnesson@users.noreply.github.com>
2021-05-08 18:53:22 -0700

    Add files via upload

--- SRPforce.m
+++ SRPforce.m
@@ -1,4 +1,4 @@
-function F_SRP = SRPforce(R,N,theta_prime,q)
+function F_SRP = SRPforce(R,N,phi_prime,q)
 %SRPFORCE(R,N,theta,Q) calculates the solar radiation pressure force depending on 
 %the area of ilumination where R is the radius of the spherical sail, N the
 %number of rays, theta [deg]  angle of the suns rays wrt the laser force
@@ -7,7 +7,7 @@ function F_SRP = SRPforce(R,N,theta_prime,q)
 
 c = 3e8;         %speed of light [m/s]
 A_s = pi*R^2;    %cross sectional area of the sunlit hemisphere of the sail [m^2]
-phi = 1366;      %solar constant adjusted for actual distance from the sun [W/m^2]
+psi = 1366;      %solar constant adjusted for actual distance from the sun [W/m^2]
 
 % finding the area per ray
 A_dy = A_s/N;
@@ -19,8 +19,8 @@ i = 1;
 % For loop used to calculate the force due to the solar radiation
 for j = alpha(1,:)
     % Calculating the force due to the ray of interest
-    SRPvec(1,i) = (phi/c)*A_dy*(1+q).*cosd(j).*cosd(j); 
-    SRPvec(2,i) = (phi/c)*A_dy*(1+q).*cosd(j).*sind(j); 
+    SRPvec(1,i) = (psi/c)*A_dy*(1+q).*cosd(j).*cosd(j); 
+    SRPvec(2,i) = (psi/c)*A_dy*(1+q).*cosd(j).*sind(j); 
     i = i + 1;
 end
 
@@ -28,6 +28,6 @@ end
 SRPvecTOT = [sum(SRPvec(1,:)) sum(SRPvec(2,:))];
 
 % resultnat SRP force vector wrt to sail refernce frame
-F_SRP = [norm(SRPvecTOT)*cosd(theta_prime) norm(SRPvecTOT)*sind(theta_prime)];
+F_SRP = [norm(SRPvecTOT)*cosd(phi_prime) norm(SRPvecTOT)*sind(phi_prime)];
 
 end
